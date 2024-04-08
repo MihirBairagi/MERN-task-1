32d4f17aa5aebbf3a3664fa6c740edb262008e2d
MihirBairagi <mihirbairagi10@gmail.com>
2024-04-08 14:34:03 +0530

    port change

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-const app = express();
+const app = express(); 
 const port = process.env.PORT || 5000;
 
 
